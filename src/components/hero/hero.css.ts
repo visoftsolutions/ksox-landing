92c2d8536821774d06be0dd361a9b92eede387f5
VerzoToKumpel <verzo.of@gmail.com>
2023-10-19 20:16:37 +0200

    moved hero to vanilla-extract

--- src/components/hero/hero.css.ts
+++ src/components/hero/hero.css.ts
@@ -9,17 +9,121 @@ export const hero = style({
   },
 });
 
-export const buttonText1 = style({
+export const heroButton = style({
   borderWidth: "2px",
   paddingTop: "4px",
   paddingBottom: "4px",
-  paddingLeft: "20px",
-  paddingRight: "20px",
+  paddingLeft: "10px",
+  paddingRight: "10px",
   borderRadius: "9999px",
-  fontSize: "20px",
+  fontSize: "16px",
   transition: "background-color",
   transitionDuration: "300ms",
   ":hover": {
     backgroundColor: "rgb(17,24,39)",
+  },
+  "@media": {
+    "screen and (min-width: 1024px)": {
+      fontSize: "20px",
+    },
+  },
+});
+
+export const heroWrapper = style({
+  maxWidth: "1270px",
+  marginLeft: "auto",
+  marginRight: "auto",
+  paddingRight: "20px",
+  paddingLeft: "20px",
+  paddingTop: "208px",
+  paddingBottom: "208px",
+  display: "grid",
+  "@media": {
+    "screen and (min-width: 1024px)": {
+      paddingRight: "32px",
+      paddingLeft: "32px",
+      paddingTop: "384px",
+      paddingBottom: "384px",
+    },
+  },
+});
+
+export const contentWrapper = style({
+  marginLeft: "auto",
+  marginRight: "auto",
+  display: "grid",
+  gridTemplateColumns: "repeat(0, 1fr)",
+  "@media": {
+    "screen and (min-width: 1024px)": {
+      gridTemplateColumns: "repeat(2, 1fr)",
+      alignItems: "center"
+    },
+  },
+});
+
+//font-bold text-transparent text-2xl sm:text-3xl md:text-5xl bg-clip-text bg-gradient-to-b from-blue-700 to-blue-200
+export const titleText = style({
+  fontWeight: "700",
+  //color: "transparent",
+  fontSize: "24px",
+  //backgroundClip: "text",
+  //backgroundImage: "linear-gradient(to bottom, var(--tw-gradient-stops))",
+  lineHeight: "1",
+  "@media": {
+    "screen and (min-width: 824px)": {
+      fontSize: "40px",
+      paddingRight: "32px",
+      paddingLeft: "32px",
+    },
+    "screen and (min-width: 1024px)": {
+      fontSize: "56px",
+      paddingRight: "0px",
+      paddingLeft: "0px",
+    },
+  },
+});
+
+export const subText = style({
+  fontSize: "14px",
+  paddingTop: "40px",
+  paddingBottom: "40px",
+  "@media": {
+    "screen and (min-width: 824px)": {
+      fontSize: "18px",
+      paddingTop: "80px",
+      paddingBottom: "80px",
+    },
+  },
+});
+
+//pt-10 lg:pt-0 flex justify-center lg:justify-end lg:mx-0 px-10 sm:px-20 lg:px-0
+export const heroImage = style({
+  paddingTop: "40px",
+  display: "grid",
+  paddingRight: "80px",
+  paddingLeft: "80px",
+  marginLeft: "auto",
+  marginRight: "auto",
+  alignItems: "center",
+  "@media": {
+    "screen and (min-width: 524px)": {
+      paddingTop: "80px",
+      paddingBottom: "80px",
+    },
+    "screen and (min-width: 1024px)": {
+      marginLeft: "0",
+      marginRight: "0",
+      paddingTop: "0px",
+      paddingBottom: "0px",
+    },
+  },
+});
+
+export const image = style({
+  objectFit: "contain",
+  "@media": {
+    "screen and (min-width: 1024px)": {
+      position: "absolute",
+    },
   },
 });
