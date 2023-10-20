cb6e8e4974d07cfa2dd06a66b184f1e9f4b29681
verzotokumpel <verzo.of@gmail.com>
2023-10-20 23:37:28 +0200

    test

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -29,7 +29,7 @@ export default component$(() => {
         link2="https://app.ksox.finance/"
       />
 
-      <div class="py-20 bg-white text-[#1D1D1D]" id="about">
+      <div class="py-20 bg-gray-50 text-[#1D1D1D]" id="about">
         <div class="max-w-[1270px] mx-auto px-8 md:px-20 lg:px-0">
           <h2 class="lg:pl-10 font-bold text-2xl sm:text-4xl text-center lg:text-start">
             Discover KSOX Products
